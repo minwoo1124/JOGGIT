404ac5b5de999b356252abe2d409976f5fd09d38
Penguin <rohgo11@gmail.com>
2022-04-20 15:30:06 +0900

    Feat : Practice_1_draw star 실습1 별찍기

--- joggit/joggit/joggit.cpp
+++ joggit/joggit/joggit.cpp
@@ -2,10 +2,36 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
     std::cout << "Hello Collaboration\n";
+  /* 별을 찍어보자
+       *
+       **
+       ***
+       ****
+       *****
+       .
+       .
+       .
+       
+       */
+    int input_num; 
+    cout << "숫자를 입력하시오." << endl; 
+    cin >> input_num;   //원하는 반복수만큼 출력할수 있게 입력
+    for (int i = 0; i < input_num; i++) //입력한 수 만큼 세로로 출력하게 반복
+    {
+        for (int j = 0; j <= i; j++) // 입력한 수 만큼 별이 출력하게 반복
+        {
+            if(i >= j)  //조건문을 씀으로서 별의 갯수를 조정
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
