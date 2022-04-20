f15f9eef741a3bd50b881b1cd1f98f2c5be00f9c
hongsw <gkenfktm@gmail.com>
2022-04-20 16:01:27 +0900

    help my code

--- joggit/joggit/joggit.cpp
+++ joggit/joggit/joggit.cpp
@@ -6,6 +6,21 @@ using namespace std;
 
 int main()
 {
- cout << "git 너무 어려워욥";
+	srand(time(NULL));
+	int lotto_ball = 7;
+	int select_lotto[7] = {};
+	cout << "----lotto 번호 뽑기----" << endl;
+	for (int i = 0; i < 7; i++)
+	{
+		int lotto_num = 1 + rand() % ((45 - 1) + 1);
+		/*if (select_lotto[i] == lotto_num)
+		{
+			int lotto_num = 1 + rand() % ((45 - 1) + 1);
+		}*/
+		select_lotto[i] = lotto_num;
+		cout << select_lotto[i] << "\t";
+	}
+	// 이거 중복안나오게 하는법 어케해요 알려주세요 고수님
+	cout << endl;
 }
 
